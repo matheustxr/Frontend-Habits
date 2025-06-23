5f78e31c6e3c4b92fb12474c472939829ca787f5
Matheus Teixeira <matheustxr.dev@gmail.com>
2025-06-23 11:44:19 -0300

    feat: install TailWind and create Dashboard page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DashboardComponent } from "./pages/dashboard/dashboard.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, DashboardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'front-habits';
+  title = 'Habits';
 }
