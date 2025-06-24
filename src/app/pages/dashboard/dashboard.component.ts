77b6bb46d642bc29a8f7fbdc2b49d50262a456e8
Matheus Teixeira <matheustxr.dev@gmail.com>
2025-06-24 19:21:40 -0300

    feat: add styles for compoonents and add lib dayjs

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../_components/header/header.component";
 import { ProgressBarComponent } from "../../_components/progress-bar/progress-bar.component";
+import { HabitDayComponent } from "../../_components/habit-day/habit-day.component";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [HeaderComponent, ProgressBarComponent],
+  imports: [HeaderComponent, ProgressBarComponent, HabitDayComponent],
   templateUrl: './dashboard.component.html',
   styles: ``
 })
 export class DashboardComponent {
   weekDays = ["D", "S", "T", "Q", "Q", "S", "S"]
 
+  mockDate = new Date(2025, 5, 21);
 }
