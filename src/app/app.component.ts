0da1879938e443dc16cf637e1a314f9ce31b121e
Matheus Teixeira <matheustxr.dev@gmail.com>
2025-07-02 19:10:21 -0300

    feat: created ome page and configuration rouutes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
-import { DashboardComponent } from "./pages/dashboard/dashboard.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, DashboardComponent],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
